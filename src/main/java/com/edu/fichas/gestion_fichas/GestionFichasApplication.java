5c326e5be2a6adfd3bfe702046fe9b519fca2b4a
JoseCochachi <josefabiancochachilopez@gmail.com>
2025-08-10 12:47:50 -0500

    first commit

---
+++ src/main/java/com/edu/fichas/gestion_fichas/GestionFichasApplication.java
@@ -0,0 +1,13 @@
+package com.edu.fichas.gestion_fichas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFichasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFichasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/fichas/gestion_fichas/GestionFichasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.fichas.gestion_fichas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFichasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
